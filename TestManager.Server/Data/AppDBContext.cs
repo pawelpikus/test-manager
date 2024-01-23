468607f4aa18d56ed47a75a67c5f984f91346a1d
Pawel Pikus (CW) <pawel.pikus@wdc.com>
2024-01-23 14:43:58 +0100

    Add models, controllers and migrate db

---
+++ TestManager.Server/Data/AppDBContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestManager.Server.Data.Models;
+
+namespace TestManager.Server.Data
+{
+    public class AppDBContext(DbContextOptions<AppDBContext> options) : DbContext(options)
+    {
+        public DbSet<Test> Tests { get; set; }
+    }
+}
---
+++ TestManager.Server/Data/Models/Test.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+namespace TestManager.Server.Data.Models
+{
+    public class Test
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [MaxLength(50), Column(TypeName = "nvarchar(50)")]
+        public string FwVersion { get; set; } = string.Empty;
+
+        [MaxLength(150), Column(TypeName = "nvarchar(150)")]
+        public string RackName { get; set; } = string.Empty;
+
+        [MaxLength(300), Column(TypeName = "nvarchar(300)")]
+        public string Title { get; set; } = "Title";
+
+        [MaxLength(150), Column(TypeName = "nvarchar(150)")]
+        public string Status { get; set; } = "Status";
+
+        [AllowNull]
+        public DateTime KickOffTimeStamp { get; set; } = DateTime.Now;
+        [AllowNull]
+        public DateTime LastModifiedTimeStamp { get; set;} = DateTime.Now;
+        
+        [MaxLength(10), Column(TypeName = "nvarchar(10)")]
+        public string Time { get; set; } = "12:30";
+
+
+    }
+}
